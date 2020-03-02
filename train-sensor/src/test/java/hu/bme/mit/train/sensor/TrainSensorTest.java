eae82730db9c37ed7c2c51462194f3db17efa4a0
Your Name <HMGG@github.com>
2020-03-02 16:06:32 +0100

    created unit test

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,5 +1,9 @@
 package hu.bme.mit.train.sensor;
 
+import hu.bme.mit.train.controller.TrainControllerImpl;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.user.TrainUserImpl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,13 +11,18 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+    TrainSensorImpl tsi;
     @Before
     public void before() {
         // TODO Add initializations
+        TrainControllerImpl tc = new TrainControllerImpl();
+        tsi = new TrainSensorImpl(tc, new TrainUserImpl(tc));
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
         // TODO Delete this and add test cases based on the issues
+        tsi.overrideSpeedLimit(10);
+        Assert.assertEquals(tsi.getSpeedLimit(), 10);
     }
 }
