24d281c87e2abb63a1bed6ae8e936dd2a8f4444f
Your Name <HMGG@github.com>
2020-03-02 16:26:40 +0100

    new unit test

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,24 +1,18 @@
 package hu.bme.mit.train.sensor;
 
-import hu.bme.mit.train.user.TrainUserImpl;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
-    TrainSensorImpl tsi;
     @Before
     public void before() {
         // TODO Add initializations
-        tsi = new TrainSensorImpl(null, new TrainUserImpl(null));
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
         // TODO Delete this and add test cases based on the issues
-        tsi.overrideSpeedLimit(10);
-        Assert.assertEquals(tsi.getSpeedLimit(), 10);
     }
 }
