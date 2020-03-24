9182ded278956ea20f08043f3f52bfe149a6cd14
ricsi98 <98kissricsi@gmail.com>
2020-03-24 13:50:43 +0100

    timer implementation

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,11 +2,25 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
+
+	public TrainControllerImpl() {
+		Timer timer = new Timer("Timer");
+		long delay = 1000L;
+		timer.schedule(new TimerTask() {
+			public void run() {
+				followSpeed();
+			}
+		}, delay);
+	}
 
 	@Override
 	public void followSpeed() {
